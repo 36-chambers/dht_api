50e058627e6d4601ab80127802f483b21877d60a
Brett <160749165+RohirrimRider@users.noreply.github.com>
2024-03-20 21:25:05 -0500

    configure

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 # this file runs the fastapi under dht_api/app.py with uvicorn
 
 import uvicorn
+from os import environ
+
+WORKERS = int(environ.get("WORKERS") or "2")
+PORT = int(environ.get("PORT") or "8000")
+HOST = environ.get("HOST") or "0.0.0.0"
 
 if __name__ == "__main__":
-    uvicorn.run("dht_api.api:app", host="0.0.0.0", port=8000, reload=False, workers=10)
+    uvicorn.run("dht_api.api:app", host=HOST, port=PORT, reload=False, workers=WORKERS)
